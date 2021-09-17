3a71a3de62052263e383a2faa3d9f3388832dfc5
kubot080301 <kubot100601@gmail.com>
2021-09-17 11:16:00 +0800

    udpate kubot_install ros

--- tools/kubot_installer/kubot_install_ros1.sh
+++ tools/kubot_installer/kubot_install_ros1.sh
@@ -6,7 +6,7 @@
 
 # Check System Version
 tput setaf 2
-echo "Check System Version"
+echo "Check System Version..."
 tput sgr0
 
 SYS_VERSION=$(lsb_release -sc)
@@ -19,11 +19,11 @@ tput sgr0
 
 # Check ROS1 Version
 tput setaf 2
-echo "Check ROS1 Version"
+echo "Check ROS1 Version..."
 tput sgr0
 
 tput setaf 3
-if [ "$SYS_VERSION" = "xenial"]; then
+if [ "$SYS_VERSION" = "xenial" ]; then
     ROS_VERSION="kinetic"
     echo "ROS_Version:" $ROS_VERSION
 elif [ "$SYS_VERSION" = "bionic" ]; then
@@ -39,92 +39,91 @@ fi
 tput sgr0
 
 # Let's start installing ROS!
-tput setaf 3
-echo "Let's start installing ROS1"
+tput setaf 5
+echo "Let's start installing ROS1..."
 tput sgr0
 
-if ["$ROS_INSTALLED" = 1]; then
+# Adding repository and source list
+tput setaf 2
+echo "Adding repository and source list..."
+tput sgr0
 
-    # Adding repository and source list
+sudo apt-add-repository universe
+sudo apt-add-repository multiverse
+sudo apt-add-repository restricted
+
+if [ "$ROS_VERSION" = "melodic" ]; then
+
+    # Setup sources.lst
     tput setaf 2
-    echo "Adding repository and source list"
+    echo "Setup sources.lst..."
     tput sgr0
 
-    sudo apt-add-repository universe
-    sudo apt-add-repository multiverse
-    sudo apt-add-repository restricted
-
-    if ["$ROS_VERSION" = "melodic" ]; then
-
-        # Setup sources.lst
-        tput setaf 2
-        echo "Setup sources.lst"
-        tput sgr0 
-
-        if ["$SYS_KERNEL" = arm]; then
-            sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-        else
-            sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.tuna.tsinghua.edu.cn/ros/ubuntu/ `lsb_release -cs` main" > /etc/apt/sources.list.d/ros-latest.list'
-            sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-        fi
-
-        # Setup keys
-        sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-        
-        # Updating apt-get
-        tput setaf 2
-        echo "Updating apt-get"
-        tput sgr0
-
-        sudo apt-get update
-
-        # Choes ros package
-        echo -e "\033[1;34m Please specify ROS package:\033[1;32m
-            1 : Desktop-Full Install (ROS, rqt, rviz, robot-generic libraries, 2D/3D simulators and 2D/3D perception)
-            2 : Desktop Install (ROS, rqt, rviz, and robot-generic libraries)
-            3 : ROS-Base (ROS package, build, and communication libraries. No GUI tools.)
-        \033[1;34m (or other for user defined) \033[1;33m"
-
-        read -p "" ROS_TOOL
-
-        if ["$ROS_TOOL" = "1"]; then
-            sudo apt-get install ros-${ROS_VERSION}-desktop-full -y
-        elif ["$ROS_TOOL" = "2"]; then
-            sudo apt-get install ros-${ROS_VERSION}-desktop -y
-        elif ["$ROS_TOOL" = "3"]; then
-            sudo apt-get install ros-${ROS_VERSION}-base -y
-        else
-            sudo apt-get install ros-${ROS_VERSION}-base -y
-        fi
-
-        # Initialize rosdep
-        tput setaf 2
-        echo "Installing rosdep"
-        tput sgr0        
-
-        sudo apt-get install python-rosdep -y
-
-        # Initialize rosdep
-        tput setaf 2
-        echo "Initializaing rosdep"
-        tput sgr0
-
-        sudo rosdep init
-        rosdep update
-
-        # Install rosinstall
-        tput setaf 2
-        echo "Installing rosinstall tools"
-        tput sgr0
-
-        sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
-
-        tput setaf 3
-        echo "Finish Install ROS1"
-        tput sgr0
+    if [ "$SYS_KERNEL" = arm ]; then
+        sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     else
-        exit
+        sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.tuna.tsinghua.edu.cn/ros/ubuntu/ `lsb_release -cs` main" > /etc/apt/sources.list.d/ros-latest.list'
+        sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
     fi
+
+    # Setup keys
+    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+    # Updating apt-get
+    tput setaf 2
+    echo "Updating apt-get..."
+    tput sgr0
+
+    sudo apt-get update
+
+    # Choes ros package
+    tput setaf 2
+    echo "Please specify ROS package:"
+    tput setaf 3
+    echo "
+    1 : Desktop-Full Install (ROS, rqt, rviz, robot-generic libraries, 2D/3D simulators and 2D/3D perception)
+    2 : Desktop Install (ROS, rqt, rviz, and robot-generic libraries)
+    3 : ROS-Base (ROS package, build, and communication libraries. No GUI tools.)
+    "
+    tput sgr0
+
+    read -p "" ROS_TOOL
+
+    if [ "$ROS_TOOL" = "1" ]; then
+        sudo apt-get install ros-${ROS_VERSION}-desktop-full -y
+    elif [ "$ROS_TOOL" = "2" ]; then
+        sudo apt-get install ros-${ROS_VERSION}-desktop -y
+    elif [ "$ROS_TOOL" = "3"] ; then
+        sudo apt-get install ros-${ROS_VERSION}-base -y
+    else
+        sudo apt-get install ros-${ROS_VERSION}-base -y
+    fi
+
+    # Initialize rosdep
+    tput setaf 2
+    echo "Installing rosdep"
+    tput sgr0
+
+    sudo apt-get install python-rosdep -y
+
+    # Initialize rosdep
+    tput setaf 2
+    echo "Initializaing rosdep"
+    tput sgr0
+
+    sudo rosdep init
+    rosdep update
+
+    # Install rosinstall
+    tput setaf 2
+    echo "Installing rosinstall tools"
+    tput sgr0
+
+    sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+
+    tput setaf 3
+    echo "Finish Install ROS1"
+    tput sgr0
 else
     exit
 fi
